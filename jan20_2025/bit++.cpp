507fc5e9b0fb214acea90abc5a6207d01f42b036
Coder-X15 <samrubenabraham@gmail.com>
2025-01-20 23:43:07 +0530

    Added README

--- jan20_2025/bit++.cpp
+++ jan20_2025/bit++.cpp
@@ -3,6 +3,12 @@
 # include <stack>
 using namespace std;
 // since this problem looks like a programming langauge thing, I think I should work it out that way
+// Our approach here: we dgaf to the syntax as of now, we're tryna pick out the operations apart from
+// the variable and then run it
+
+// Challenges for you: 
+// 1. Make a validation function that ensures that the statement follows the right syntax.
+// 2. Make the program simpler
 
 stack<string> opstack;
 
